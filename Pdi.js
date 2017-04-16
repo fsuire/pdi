331a83de64eeb6e38bb96caf9d600ce9afa765f6
Frédéric Suire <fred_suire@yahoo.fr>
2017-04-16 18:57:45 +0200

    cache feature

--- Pdi.js
+++ Pdi.js
@@ -6,26 +6,69 @@ class Pdi {
 
   constructor(rootPath) {
     this._rootPath = rootPath;
+    this._cache = {};
   }
 
   get(what, options) {
+    if(Array.isArray(what)) {
+      return Promise.all(what.map(serviceName => {
+        return this._getOneService(serviceName, options);
+      }));
+    } else if(typeof what === 'object') {
+      return Promise.all(
+        Object.keys(what).map(serviceName => {
+          return this._getOneService(what[serviceName], options);
+        })
+      ).then(instances => {
+        return Object.keys(what)
+          .reduce((acc, value, index) => {;
+            acc[value] = instances[index];
+            return acc;
+          }, {});
+      });
+    } else {
+      return this._getOneService(what, options);
+    }
+  }
+
+  _getOneService(what, options) {
     return new Promise((resolve, reject) => {
-      try {
-        const rawService = req(`${this._rootPath}/${what}`);
-        const service = rawService();
-        if(service instanceof Promise) {
-          service.then(serviceInstance => {
-            resolve(serviceInstance);
-          }).catch(err => {
-            reject(err);
-          });
-        } else {
-          resolve(service);
-        }
-      } catch(err) {
-        reject(err);
+      const cachedServiceInstance = this._getInCache(what);
+      if(cachedServiceInstance) {
+        resolve(cachedServiceInstance);
+      } else {
+        this._createService(what, resolve, reject);
       }
     });
+  }
+
+  _getInCache(what) {
+    if(typeof this._cache[what] !== 'undefined') {
+      return this._cache[what];
+    }
+    return undefined;
+  }
+
+  _createService(what, resolve, reject) {
+    const serviceFactory = req(`${this._rootPath}/${what}`);
+    const service = serviceFactory();
+    const passToCache = serviceInstance => {
+      if(serviceFactory.cache) {
+        this._cache[what] = serviceInstance;
+      }
+    };
+
+    if(service instanceof Promise) {
+      service.then(serviceInstance => {
+        passToCache(serviceInstance);
+        resolve(serviceInstance);
+      }).catch(err => {
+        reject(err);
+      });
+    } else {
+      passToCache(service);
+      resolve(service);
+    }
   }
 
 }
--- Pdi.test.js
+++ Pdi.test.js
@@ -6,42 +6,111 @@ const Pdi = require('./Pdi');
 
 describe('Pdi', () => {
 
-  it('should get a service instance', () => {
-    const serviceFactoryMock = jest.fn((...dependencies) => 'test');
-    reqMock.mockImplementationOnce(() => serviceFactoryMock);
-    const pdi = new Pdi('serviceRoot');
-    return pdi.get('service').then(service => {
-      expect(service).toBe('test');
-      expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+  let pdi;
+
+  beforeEach(() => {
+    pdi = new Pdi('serviceRoot');
+    jest.clearAllMocks();
+  });
+
+  describe('instance', () => {
+    it('should get a service instance', () => {
+      const serviceFactoryMock = jest.fn((...dependencies) => 'test');
+      reqMock.mockImplementationOnce(() => serviceFactoryMock);
+      return pdi.get('service').then(service => {
+        expect(service).toBe('test');
+        expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+      });
+    });
+
+    it('should get a service instance even if the serviceFactory returns a promise', () => {
+      const serviceFactoryMock = jest.fn((...dependencies) => Promise.resolve('test'));
+      reqMock.mockImplementationOnce(() => serviceFactoryMock);
+      return pdi.get('service').then(service => {
+        expect(service).toBe('test');
+        expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+      });
+    });
+
+    it('should not get a service instance', () => {
+      reqMock.mockImplementationOnce(() => {throw 'test error';});
+      return pdi.get('service').catch(error => {
+        expect(error).toBe('test error');
+        expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+      });
+    });
+
+    it('should not get a service instance if the serviceFactory returns a failing promise', () => {
+      const serviceFactoryMock = jest.fn((...dependencies) => Promise.reject('test error'));
+      reqMock.mockImplementationOnce(() => serviceFactoryMock);
+      return pdi.get('service').catch(error => {
+        expect(error).toBe('test error');
+        expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+      });
     });
   });
 
-  it('should get a service instance even if the serviceFactory returns a promise', () => {
-    const serviceFactoryMock = jest.fn((...dependencies) => Promise.resolve('test'));
-    reqMock.mockImplementationOnce(() => serviceFactoryMock);
-    const pdi = new Pdi('serviceRoot');
-    return pdi.get('service').then(service => {
-      expect(service).toBe('test');
-      expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+  describe('several instances at once', () => {
+    const helloFactoryMock = jest.fn((...dependencies) => Promise.resolve('Hello'));
+    const worldFactoryMock = jest.fn((...dependencies) => 'World');
+
+    beforeEach(() => {
+      reqMock.mockImplementation(() => {
+        const lastCallArgs = reqMock.mock.calls[reqMock.mock.calls.length - 1];
+        if (lastCallArgs[0] === 'serviceRoot/hello') {
+          return helloFactoryMock;
+        } else if (lastCallArgs[0] === 'serviceRoot/other/world') {
+          return worldFactoryMock;
+        }
+      });
+    });
+
+    it('should get several services instance from an array', () => {
+      return pdi.get(['hello', 'other/world']).then(([hello, world]) => {
+        expect(hello).toBe('Hello');
+        expect(world).toBe('World');
+      });
+    });
+
+    it('should get several services instance from an object', () => {
+      return pdi.get({
+        hello: 'hello',
+        world: 'other/world'
+      }).then(({hello, world}) => {
+        expect(hello).toBe('Hello');
+        expect(world).toBe('World');
+      });
     });
   });
 
-  it('should not get a service instance', () => {
-    reqMock.mockImplementationOnce(() => {throw 'test error';});
-    const pdi = new Pdi('serviceRoot');
-    return pdi.get('service').catch(error => {
-      expect(error).toBe('test error');
-      expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
-    });
-  });
+  describe('cache', () => {
+    const serviceFactoryMock = jest.fn((...dependencies) => Math.floor(Math.random() * 1000));
 
-  it('should not get a service instance if the serviceFactory returns a failing promise', () => {
-    const serviceFactoryMock = jest.fn((...dependencies) => Promise.reject('test error'));
-    reqMock.mockImplementationOnce(() => serviceFactoryMock);
-    const pdi = new Pdi('serviceRoot');
-    return pdi.get('service').catch(error => {
-      expect(error).toBe('test error');
-      expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+    it('should get a cached service instance', () => {
+      serviceFactoryMock.cache = true;
+      reqMock.mockImplementationOnce(() => serviceFactoryMock);
+      let firstObtainedInstance;
+      return pdi.get('service').then(service => {
+        firstObtainedInstance = service;
+        expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+        return pdi.get('service');
+      }).then(service => {
+        expect(service).toEqual(firstObtainedInstance);
+      });
+    });
+
+    it('should get a not cached service instance', () => {
+      serviceFactoryMock.cache = false;
+      reqMock.mockImplementationOnce(() => serviceFactoryMock);
+      reqMock.mockImplementationOnce(() => serviceFactoryMock);
+      let firstObtainedInstance;
+      return pdi.get('service').then(service => {
+        firstObtainedInstance = service;
+        expect(reqMock).toHaveBeenCalledWith('serviceRoot/service');
+        return pdi.get('service');
+      }).then(service => {
+        expect(service).not.toEqual(firstObtainedInstance);
+      });
     });
   });
 
